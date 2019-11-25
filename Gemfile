99ac1acae8577fc914b6aa20a0d2891e2af0dcc7
braz <gvcerveirabraz@gmail.com>
2019-11-25 11:39:32 -0300

    Styled a litle bit

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 #gem for sign up
 gem 'clearance' , '~> 1.15.1'
-
+gem 'bootstrap-sass', '~> 3.4.1'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def alert_class (flash_type)
+        case flash_type.to_sym
+        when :notice
+            "alert-sucess"
+        when :alert
+            "alert-warning"
+        when :error
+            "aler-danger"
+        end
+    end
 end
